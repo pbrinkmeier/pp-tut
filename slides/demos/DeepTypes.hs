0e5ed1532e0c228f5f9506f02cfd4d1df1cee701
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2019-11-05 11:29:14 +0100

    Smaller changes

--- slides/demos/DeepTypes.hs
+++ slides/demos/DeepTypes.hs
@@ -1,14 +1,14 @@
 module DeepTypes where
 
-vA  = (\x -> x)     :: a -> a
-vB  = (\g x -> g x) :: (a -> b) -> a -> b
-vC  = vB            :: (a -> b) -> (a -> b)
-vD  = (\x _ -> x)   :: (a -> b -> a)
-vE1 = (\x _ -> x)   :: (a -> a -> a)
-vE2 = (\_ y -> y)   :: (a -> a -> a)
-vF  = []            :: [a]
-vG  = [1]           :: [Int]
-vH  = []            :: [[a]]
-vI  = [[]]          :: [[a]]
+vA  = (\x -> x)   :: a -> a
+vB  = (\x -> x)   :: (a -> b) -> a -> b
+vC  = (\x -> x)   :: (a -> b) -> (a -> b)
+vD  = (\x _ -> x) :: (a -> b -> a)
+vE1 = (\x _ -> x) :: (a -> a -> a)
+vE2 = (\_ y -> y) :: (a -> a -> a)
+vF  = []          :: [a]
+vG  = [1]         :: [Int]
+vH  = []          :: [[a]]
+vI  = [[]]        :: [[a]]
 -- Alternativ:
 -- vA x = x, vB g x = g x, etc.
--- slides/demos/List.hs
+++ slides/demos/List.hs
@@ -1,6 +1,7 @@
 module List where
 
 data List a = Null | Cons a (List a)
+    deriving (Show)
 
 map' f Null = Null
-map' f (Cons x xs) = Cons x (map f xs)
+map' f (Cons x xs) = Cons (f x) (map' f xs)

01c65c90288840adf35763860a9508be693219eb
Paul Brinkmeier <paul.brinkmeier@gmail.com>
2020-11-16 21:43:39 +0100

    Tutorial 2

---
+++ demos/Hangman.hs
@@ -0,0 +1,23 @@
+module Hangman where
+
+main =
+  runConsoleGame
+  (showHangman guessWord)
+    (updateHangman guessWord)
+    initHangman
+  where
+    guessWord = "haskell"
+
+runConsoleGame :: (s -> String) -> (String -> s -> s) -> s -> IO ()
+runConsoleGame showGame updateGame gameState = do
+  putStrLn (showGame gameState)
+  inputLine <- getLine
+  runConsoleGame showGame updateGame (updateGame inputLine gameState)
+
+-- Tut-Code: --
+
+showHangman word guesses = ""
+
+initHangman = []
+
+updateHangman word inputLine guesses = guesses

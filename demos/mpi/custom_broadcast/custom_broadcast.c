1e19e4a4ec17b178c26b3fcfefd603e3b5931650
Paul Brinkmeier <pp-tut@pbrinkmeier.de>
2021-01-12 12:07:21 +0100

    Add solution for custom_Bcast

--- demos/mpi/custom_broadcast/custom_broadcast.c
+++ demos/mpi/custom_broadcast/custom_broadcast.c
@@ -3,7 +3,21 @@
 
 void custom_Bcast(void *buf, int count, MPI_Datatype type, int root, MPI_Comm comm) {
 	int unique_tag = 42;
-        // TODO
+
+        int rank, size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+        if (rank == root) {
+                for (int dest = 0; dest < size; dest++) {
+                        if (dest != root) {
+                                MPI_Send(buf, count, type, dest, unique_tag, comm);
+                        }
+                }
+        } else {
+                MPI_Status status;
+                MPI_Recv(buf, count, type, root, unique_tag, comm, &status);
+        }
 }
 
 int main(int argc, char** args) {
@@ -16,7 +30,9 @@ int main(int argc, char** args) {
 
 	printf("[%d, before Bcast]: msg = %d\n", rank, msg);
 	custom_Bcast(&msg, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(&msg, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	// Barrier is not actually needed but increases output readability
+        fflush(stdout);
 	MPI_Barrier(MPI_COMM_WORLD);
 	printf("[%d, after Bcast ]: msg = %d\n", rank, msg);
 

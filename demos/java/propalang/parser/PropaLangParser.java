7faa48d25d188913207403039ea8feb8e36f1e58
Paul Brinkmeier <hallo@pbrinkmeier.de>
2022-02-08 13:50:02 +0100

    Add while, for and divide to pplang

--- demos/java/propalang/parser/PropaLangParser.java
+++ demos/java/propalang/parser/PropaLangParser.java
@@ -131,14 +131,33 @@ public class PropaLangParser {
 
                 return new ReturnStatement(returnExpr);
 
-            // TODO: Parse while (condition) statement
             case WHILE:
-                throw this.lexer.error("While not implemented");
+                this.expect(TokenType.WHILE);
+                this.expect(TokenType.LEFT_PAREN);
+                Expression loopCondition = this.parseExpression();
+                this.expect(TokenType.RIGHT_PAREN);
+                Statement loopBody = this.parseStatement();
 
-            // TODO: Parse for (assign; condition; assign) statement
-            // Check cases IF and IDENTIIER above for hints :)
+                return new WhileStatement(loopCondition, loopBody);
+
             case FOR:
-                throw this.lexer.error("For not implemented");
+                this.expect(TokenType.FOR);
+                this.expect(TokenType.LEFT_PAREN);
+                Statement init = this.parseAssigment();
+                this.expect(TokenType.SEMICOLON);
+                Expression forCondition = this.parseExpression();
+                this.expect(TokenType.SEMICOLON);
+                Statement post = this.parseAssigment();
+                this.expect(TokenType.RIGHT_PAREN);
+                Statement forBody = this.parseStatement();
+
+                return new BlockStatement(new Block(Arrays.asList(
+                    init,
+                    new WhileStatement(forCondition, new BlockStatement(new Block(Arrays.asList(
+                        forBody,
+                        post
+                    ))))
+                )));
 
             default:
                 throw this.lexer.error(String.format("Statement expected at token %s", this.token));
@@ -195,6 +214,9 @@ public class PropaLangParser {
             if (this.check(TokenType.STAR)) {
                 this.expect(TokenType.STAR);
                 lhs = new OperatorExpression(Operator.MULTIPLY, lhs, this.parseFactor());
+            } else if (this.check(TokenType.SLASH)) {
+                this.expect(TokenType.SLASH);
+                lhs = new OperatorExpression(Operator.DIVIDE, lhs, this.parseFactor());
             } else {
                 break;
             }

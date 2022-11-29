e28c58e54ba246d3127ab14dce07ee2c6bef1ccf
Paul Brinkmeier <hallo@pbrinkmeier.de>
2022-11-29 11:42:43 +0100

    Add tut 4 Haskell notes

--- demos/Tut04.hs
+++ demos/Tut04.hs
@@ -1,5 +1,40 @@
 module Tut04 where
 
+import Data.Set (Set, fromList, union, delete)
+
+data LambdaTerm
+    = Var String
+    | App LambdaTerm LambdaTerm
+    | Abs String LambdaTerm
+
+lamId = Abs "x" (Var "x")
+lamComp = Abs "f" (Abs "g" (Abs "x" (App (Var "f") (App (Var "g") (Var "x")))))
+lamEvalAtX = Abs "f" (Var "f" `App` Var "x")
+
+instance Show LambdaTerm where
+    show (Var s) = s
+    show (App f a) = "(" ++ show f ++ ") (" ++ show a ++ ")"
+    show (Abs p b) = "\\" ++ p ++ "." ++ show b
+
+fv :: LambdaTerm -> Set String
+fv (Var s) = fromList [s]
+fv (App f a) = fv f `union` fv a
+fv (Abs p b) = delete p (fv b)
+
+substitute :: (String, LambdaTerm) -> LambdaTerm -> LambdaTerm
+substitute (_, b) _
+    | fv b /= fromList [] = error "Term contains free vars"
+substitute (a, b) (Var v)
+    | a == v = b
+    | otherwise = Var v
+substitute sub (App f x) =
+    App (substitute sub f) (substitute sub x)
+substitute (a, b) (Abs p c)
+    | a == p = Abs p c
+    | otherwise = Abs p (substitute (a, b) c)
+
+-- Wiederholung
+
 fibs :: [Integer]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
